f4e91332dbad65f1a80b4c364c0bc11deef6fcd3
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-09-28 10:59:35 +0100

    Typewriter and dropdown
    completed typewriter effect with javascript and dropdown menu, now applying CSS and javascript.

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,12 +1,12 @@
 //variables
-let titleArray = ["My Name is Amy Vincent"];
+let titleArray = ["My Name is Amy Vincent, I am a Web Developer"];
 let textPosition = 0;
 let speed = 100;
 
 //function
 typewriter = () =>
 {
-    document.querySelector(".img-title").
+    document.querySelector("#img-title").
     innerHTML = titleArray[0].substring(0, textPosition)
     + "<span>\u25ae</span>";// text block
    //as long as text position isnt += array length it will keep incrementing
