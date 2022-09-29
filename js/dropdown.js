7cf370f9c2616c457978c277cb2f0816cb75651a
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-09-29 10:57:05 +0100

    dropdown
    added slideout animation to javascript

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,1 +1,14 @@
-$(".dropdown-animsition").animsition();
+// opens drop down menu on button click
+
+$(".dropbtn").click(function()
+{
+    $(".dropdown-content").slideDown();
+    callback()
+})
+
+//closes when link is pressed 
+
+$(".dropdown-content a").click(function()
+{
+     $(".dropdown-content").slideUp(1000);
+});
