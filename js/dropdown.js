3827b6369bd53db8a75c43a110cc39d9a5a92b6c
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-09-30 12:34:14 +0100

    sticky nav
    attempting to make navigation bar sticky

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,23 +1,26 @@
 
 $(document).ready(function(){
-    $(".dropbtn").click(function(){
-            $(".dropdown-content").slideToggle();
-            $(".dropdown-content ")
-        });
+ 
+    
+        $(".dropbtn").click(function(){
+                $(".dropdown-content").slideToggle();
+                $(".dropdown-content ")
+            });
 
-        // $("ul li").click(function(){
-        //     $("ul ul").slideUp();
-        //     $(this).find('ul').slideToggle();
-        // });
-        $('.dropdown-content a').click(function () {
-            $(this).siblings().find('.dropdown-content').slideUp();
-            $(this).find('.dropdown-content').slideToggle();
-        });
+            // $("ul li").click(function(){
+            //     $("ul ul").slideUp();
+            //     $(this).find('ul').slideToggle();
+            // });
+            $('.dropdown-content a').click(function () {
+                $(this).siblings().find('.dropdown-content').slideUp();
+                $(this).find('.dropdown-content').slideToggle();
+            });
 
-        $(window).resize(function(){
-            if($(window).width() > 768){
-                $(".dropdown-content").removeAttr('style');
-            }
-        });
+            $(window).resize(function(){
+                if($(window).width() > 768){
+                    $(".dropdown-content").removeAttr('style');
+                }
+            });
+    
  });
 
