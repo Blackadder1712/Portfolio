9092961da23a7bd5678aa34f93f8db225599ba79
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-09-30 10:50:12 +0100

    Contact form
    Have completed the contact form with correct regex for each input field.

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,14 @@
-// opens drop down menu on button click
+
+
 
 $(".dropbtn").click(function()
 {
     $(".dropdown-content").slideDown();
     callback()
-})
+});
+
+$(.document).ready(function(){
+    $(".dropdown").scroll(function(){
+       $(".dropdown").css('display','inline').fadeOut(1000);
+    });
+ });
--- js/email-val.js
+++ js/email-val.js
@@ -1,5 +1,11 @@
-const emailInput = document.getElementById('email');
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let emailInput = document.getElementById('email');
+let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let nameInput = document.getElementById('Name');
+let nameRegex = /[^0-9]/;
+let lastNameInput = document.getElementById('LastName');
+let LastnameRegex = /[^0-9]/;
+let messageInput = document.getElementById('Subject');
+let messageRegex = /(?:[A-Za-z].*?\d|\d.*?[A-Za-z])/;
 
 
 $(".submit").click(function()
@@ -8,6 +14,18 @@ $(".submit").click(function()
     {
     alert('Invalid email address.');
   
+    }
+  else if (!nameInput.value.match(nameRegex))
+    {
+      alert('Invalid First name.')
+    }
+    else if (!lastNameInput.value.match(LastnameRegex))
+    {
+      alert('Invalid Last name.')
+    }
+  else (!messageInput.value.match(messageRegex))
+    {
+      alert('Please enter message')
     }
 
 });
