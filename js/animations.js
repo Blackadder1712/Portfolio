7213dc12c048245f1e359e1cd64f91afc8edc613
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-10-04 11:44:08 +0100

    Page transitions and layout
    added a fadeIn animation so the pages have a smoother transition, also adjusted layout on scs page so it fits mobile view

---
+++ js/animations.js
@@ -0,0 +1,25 @@
+//Page fade ins 
+$(document).ready(function()
+    {
+     $(".caption-text-scs").css({opacity:0.0, visibility:"visible"}).animate({opacity: 1.0},1900);
+    });
+
+$(document).ready(function()
+    {
+    $(".img-title").css({opacity:0.0, visibility:"visible"}).animate({opacity: 1.0},1900);
+    });
+
+$(document).ready(function()
+    {
+    $(".caption-text-about").css({opacity:0.0, visibility:"visible"}).animate({opacity: 1.0},1900);
+    });
+
+
+
+    
+
+
+
+
+ 
+  
