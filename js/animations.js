1d6280f8e71095c3040969431e34345a9d29473b
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2022-10-04 12:00:49 +0100

    message field
    Fixed the message field so it now accepts multiple lines of code

--- js/animations.js
+++ js/animations.js
@@ -14,6 +14,11 @@ $(document).ready(function()
     $(".caption-text-about").css({opacity:0.0, visibility:"visible"}).animate({opacity: 1.0},1900);
     });
 
+    $(document).ready(function()
+    {
+    $("#contact-form").css({opacity:0.0, visibility:"visible"}).animate({opacity: 1.0},1900);
+    });
+
 
 
     
