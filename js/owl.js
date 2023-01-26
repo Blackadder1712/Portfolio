29a7a2d7a4f2487cd55bbd978730b3b146fee045
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-01-26 10:15:06 +0000

    carousel
    carousel visible

---
+++ js/owl.js
@@ -0,0 +1,14 @@
+$('.container').trigger('play. ".container".autoplay',[1000])
+$('.container').owlCarousel({
+   
+    items:2,
+  
+    margin:10,
+    autoPlay:true,
+    autoPlayTimeout:1000,
+    autoPlayHoverPause:true,
+    autoPlaySpeed:5000  
+   
+    
+  
+})
