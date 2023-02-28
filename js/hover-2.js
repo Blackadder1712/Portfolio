83fb3b15ef03ba00e03229e0f5e008037c3e28ca
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-02-28 21:13:16 +0000

    modals
    modals work better

--- js/hover-2.js
+++ js/hover-2.js
@@ -1,26 +1,36 @@
 // Get the modal
 var modal2 = document.getElementById("myModal-2");
+var background = document.getElementById("scroll-2");
+
 
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn-2");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-2")[0];
+var span = document.getElementsByClassName("close-2")[0]; 
 
-// When the user clicks on the button, open the modal
+// When the user clicks on the button, open the modal and background
 btn.onclick = function() {
   modal2.style.display = "block";
+  background.style.display = "flex";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal2.style.display = "none";
+  background.style.display = "none";
+}
+
+btn.onclick = function() {
+  modal2.style.display = "block";
+  background.style.display = "flex";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal2.style.display = "none";
+    background.style.display = "none";
   }
 }
