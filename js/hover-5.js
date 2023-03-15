047b48e7499a3eed29d30cd15d394fb05a048f0b
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-03-15 21:19:18 +0000

    finished in mobile view
    done

--- js/hover-5.js
+++ js/hover-5.js
@@ -1,5 +1,7 @@
 // Get the modal
+
 var modal5 = document.getElementById("myModal-5");
+var background = document.getElementById("scroll-2");
 
 
 // Get the button that opens the modal
@@ -11,16 +13,25 @@ var span = document.getElementsByClassName("close-5")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal5.style.display = "block";
+  background.style.display = "flex";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal5.style.display = "none";
+  background.style.display = "none";
+}
+
+
+btn.onclick = function() {
+  modal5.style.display = "block";
+  background.style.display = "flex";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal5) {
     modal5.style.display = "none";
+    background.style.display = "flex";
   }
 }
